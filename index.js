b8fbf73832ab77a196f01e2c9c6f9196f89bc5cd
Moritz <22394314+movd@users.noreply.github.com>
2019-10-11 14:39:34 +0200

    Finish Ex. 3.3 and 3.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -28,11 +28,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/persons", (req, res) => {
-  console.log(persons);
-  res.json(persons);
-});
-
 app.get("/info", (req, res) => {
   const timestamp = new Date();
 
@@ -40,6 +35,27 @@ app.get("/info", (req, res) => {
     `<p>Phonebook has info for ${persons.length} people</p>
     <p>${timestamp}</p>`
   );
+});
+
+app.get("/api/persons", (req, res) => {
+  console.log(persons);
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
