ca1a1d2c70795c55cc9bde53e50265c54a795724
Moritz <22394314+movd@users.noreply.github.com>
2019-10-15 14:56:22 +0200

    Finish Ex. 3.11 phonebook full stack

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My submissions for [Full Stack Open 2019 part3](https://fullstackopen.com/en/part3/) aka the Phonebook backend.
 
-This backend ist live at <https://glacial-cliffs-64027.herokuapp.com/api/persons>
+The backend is live at <https://glacial-cliffs-64027.herokuapp.com/api/persons> and the frontend at: <https://glacial-cliffs-64027.herokuapp.com/>
---
+++ build/precache-manifest.e90f1e092d65afc4cad1e87f4306aafa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8351780fcd00b1a29042d6c9d2acc208",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2937c22fefaa5e2b8a6",
+    "url": "/static/css/main.8b373033.chunk.css"
+  },
+  {
+    "revision": "023d7f53b53d7b29b35c",
+    "url": "/static/js/2.a3e5c8c9.chunk.js"
+  },
+  {
+    "revision": "d2937c22fefaa5e2b8a6",
+    "url": "/static/js/main.a5b9705a.chunk.js"
+  },
+  {
+    "revision": "e2d4c6f71ace7b0f0cca",
+    "url": "/static/js/runtime-main.558687b4.js"
+  }
+]);
